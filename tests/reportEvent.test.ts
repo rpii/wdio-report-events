ad6d9ce5e57704f07727b9ddd35ca46fb280311b
rpeter <rpeter@sandia.gov>
2020-01-16 12:26:08 -0800

    put in event sink

--- tests/reportEvent.test.ts
+++ tests/reportEvent.test.ts
@@ -3,27 +3,30 @@ import ReportEvents  from "../src/wdio-report-events" ;
 
 import  { expect } from  "chai";
 
-// TODO Work in Progress
-// saveScreenshot(filepath) {
-//     let test = this.getTest(this.testUid) ;
-//     this.moveErrorsToEvents(test) ;
-//     test.events.push({type: 'screenshot', value: filepath}) ;
-// }
-//
-// saveMessage(message) {
-//     const test = this.getTest(this.testUid);
-//     this.moveErrorsToEvents(test) ;
-//     test.events.push({type: 'log', value: message}) ;
-// }
+export class EventListener {
+
+    saveScreenshot(filepath) {
+        console.log("screenshot event fired:" + filepath) ;
+        expect(true,"screenshot event fired:" + filepath) ;
+    }
+
+    saveMessage(message) {
+        console.log("message event fired:" + message)
+        expect(true,"message event fired:" + message) ;
+    }
+}
 
 suite('test suite', function () {
-    // process.on('test:log', this.saveMessage.bind(this));
-    // process.on('test:screenshot', this.saveScreenshot.bind(this));
+    let eventSink = new EventListener() ;
+    process.on('test:log', eventSink.saveMessage.bind(this));
+    process.on('test:screenshot', eventSink.saveScreenshot.bind(this));
     test('fire log message', function () {
+        console.log("firing message event") ;
         let proxy = new ReportEvents() ;
         proxy.logMessage("This is a test log message");
     });
     test('fire log screenshot', function () {
+        console.log("firing screenshot event") ;
         let proxy = new ReportEvents() ;
         proxy.logScreenshot("test.png");
     });

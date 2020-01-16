1cbab0f59d19fbc014f25807833db93eee5a523e
rpeter <rpeter@sandia.gov>
2020-01-16 12:29:31 -0800

    cleanup output

--- tests/reportEvent.test.ts
+++ tests/reportEvent.test.ts
@@ -6,28 +6,28 @@ import  { expect } from  "chai";
 export class EventListener {
 
     saveScreenshot(filepath) {
-        console.log("screenshot event fired:" + filepath) ;
+        console.log("screenshot event sink:" + filepath) ;
         expect(true,"screenshot event fired:" + filepath) ;
     }
 
     saveMessage(message) {
-        console.log("message event fired:" + message)
+        console.log("message event sink:" + message)
         expect(true,"message event fired:" + message) ;
     }
 }
 
 suite('test suite', function () {
+    let proxy = new ReportEvents() ;
     let eventSink = new EventListener() ;
-    process.on('test:log', eventSink.saveMessage.bind(this));
-    process.on('test:screenshot', eventSink.saveScreenshot.bind(this));
+    proxy.connectMessageEvent(eventSink.saveMessage.bind(this));
+    proxy.connectScreenshotEvent(eventSink.saveScreenshot.bind(this));
     test('fire log message', function () {
         console.log("firing message event") ;
-        let proxy = new ReportEvents() ;
+
         proxy.logMessage("This is a test log message");
     });
     test('fire log screenshot', function () {
         console.log("firing screenshot event") ;
-        let proxy = new ReportEvents() ;
         proxy.logScreenshot("test.png");
     });
 });
